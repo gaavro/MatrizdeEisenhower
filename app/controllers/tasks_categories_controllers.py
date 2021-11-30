641e600efda858ceed8516f52d27a2e144bfc3b4
Gabriela Rodrigues <gabrielarodrigues432@gmail.com>
2021-11-30 17:58:09 -0300

    deleting arquivos

--- app/controllers/tasks_categories_controllers.py
+++
@@ -1,8 +0,0 @@
-from flask import request, current_app, jsonify
-from app.models.tasks_categories_model import TasksCategories
-      
-
-
-def get_taskcategories():
-    ...
-
--- app/routes/eisehowers_blueprint.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-            
-bp_eisehowers = Blueprint("bp_eisehonhowers", __name__, )
-
-# bp_eisehonhowers.post("")()
